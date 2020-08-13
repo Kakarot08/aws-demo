80fac701f458202714e00abee987e67a5dcb0184
Abhishek Dhillon <abhishek.dhillon@paxcel.net>
2020-08-13 14:29:13 +0530

    aws-configuration

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
+require("dotenv").config({path: '.env'});
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.Access_Key_ID,
+    secretAccessKey: process.env.Secret_Access_Key
 });
+
+const uploadFile = (fileName) => {
+    // Read content from the file
+    const fileContent = fs.readFileSync(fileName);
+
+    // Setting up S3 upload parameters
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: 'cap.jpg', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
+
+uploadFile('cap.jpg');
